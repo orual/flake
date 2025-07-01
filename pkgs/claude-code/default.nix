602db0504e461fd5a6539c44ffb66524005bc512
Orual <orual@nonbinary.computer>
2025-07-01 11:47:05 -0400

    backup: before package updates 2025-07-01_11:47:05

--- pkgs/claude-code/default.nix
+++ pkgs/claude-code/default.nix
@@ -6,16 +6,16 @@
 }:
 buildNpmPackage rec {
   pname = "claude-code";
-  version = "1.0.31";
+  version = "1.0.38";
 
   nodejs = nodejs_20; # required for sandboxed Nix builds on Darwin
 
   src = fetchzip {
     url = "https://registry.npmjs.org/@anthropic-ai/claude-code/-/claude-code-${version}.tgz";
-    hash = "sha256-DrjR1LHnLXk2cW3zXO1jxc3octITMq6t28E2KJHBxZE=";
+    hash = lib.fakeHash;
   };
 
-  npmDepsHash = "sha256-OO6dzPPZS+EShaUoBxFEup45M9x54Plu/UNxSFC1cmA=";
+  npmDepsHash = "";
 
   postPatch = ''
     cp ${./package-lock.json} package-lock.json
